c5edf9f7b52fe99b4713581c683876263c997462
Kyle Oda <1624udon@gmail.com>
2021-07-10 16:46:44 -0700

    finished exercise 3.15

--- index.js
+++ index.js
@@ -73,11 +73,19 @@ app.get('/api/persons/:id', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   persons = persons.filter(p => p.id !== id)
 
-  response.status(204).end()
+//   response.status(204).end()
+// })
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    // .catch(error => next(error))
 })
 
 const generateID = () => {
